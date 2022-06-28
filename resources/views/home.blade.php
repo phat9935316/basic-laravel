393faaef1c3cd2741987cf142dc3e45e33b1002a
phatpt <phatpt@kaopiz.com>
2022-06-28 17:29:59 +0900

    Update title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,17 +6,17 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-body">
-                    <a href="{{ route('b1') }}">Bài tập về  View Blade</a>
+                    <a href="{{ route('b1') }}">Bài tập về View Blade</a>
                 </div>
             </div>
             <div class="card">
                 <div class="card-body">
-                    <a href="{{ route('b2') }}">Bài tập về  Query_Builder</a>
+                    <a href="{{ route('b2') }}">Bài tập về Query_Builder</a>
                 </div>
             </div>
             <div class="card">
                 <div class="card-body">
-                    <a href="{{ route('b3') }}">Bài tập về  Eloquent ORM</a>
+                    <a href="{{ route('b3') }}">Bài tập về Eloquent ORM</a>
                 </div>
             </div>
         </div>
